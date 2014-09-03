4c6748d701381c86807cadba0a01bb289c8d8fa4
Aaron Elmore <aelmore@csail.mit.edu>
2014-09-03 12:33:05 -0400

    testing more

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/db', function (request, response) {
 app.get('/dbi', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     var r1 = 're1';
-    client.query("insert into students values('ada','dhka','"+r1"')", function(err, result) {
+    client.query("insert into students values($1, $2, $3)",['c1','c2',r1 ], function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
