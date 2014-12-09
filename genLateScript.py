2dfbf479c0189f1c7de7a805f166cbf7c9057c3f
Aaron Elmore <aelmore@csail.mit.edu>
2014-12-09 14:10:52 -0500

    wip

--- genLateScript.py
+++ genLateScript.py
@@ -6,7 +6,7 @@ gitRepos = []
 with open('students.csv','r') as studcsv:
     rdr = csv.DictReader(studcsv)
     for r in rdr:
-        gitRepos.append('./calcLateDays.sh %s "%s,%s" submitted.csv\n'% (r['UniversityID'], r['LastName'],r['FirstName']))
+        gitRepos.append('./calcLateDays.sh %s "%s, %s" submitted.csv\n'% (r['UniversityID'], r['LastName'],r['FirstName']))
 
 with open('submited.sh','w') as r:
     for l in gitRepos:
