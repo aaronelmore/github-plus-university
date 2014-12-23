46354572799867abac910df7301e85d49f637163
Aaron Elmore <aelmore@csail.mit.edu>
2014-12-23 17:25:27 -0500

    small update

--- extractRepos.py
+++ extractRepos.py
@@ -6,9 +6,9 @@ gitRepos = []
 with open('students.csv','r') as studcsv:
     rdr = csv.DictReader(studcsv)
     for r in rdr:
-        gitRepos.append('./getRepo.sh %s lab3 2014-11-02 00:15:00\n'% r['UniversityID'])
+        gitRepos.append('./getRepo.sh %s lab6 2014-12-20 00:15:00\n'% r['UniversityID'])
 
-with open('run3.sh','w') as r:
+with open('run6.sh','w') as r:
     for l in gitRepos:
         r.write(l)
 
